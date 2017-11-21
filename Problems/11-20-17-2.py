49e5f8430615f231d4a11f1b850e4d1a7e4c7e84
AnvilDev <andres.ernesto.valdes@gmail.com>
2017-11-20 20:21:55 -0500

    Solved 8.1 from CTCI.

---
+++ Problems/11-20-17-2.py
@@ -0,0 +1,10 @@
+def triple_step(n):
+    """
+    A child is running up a staircase with n steps
+    and can hop either 1 step, 2 steps, or 3 steps at a time. Implement a
+    method to count how many possible ways the child can run up the stairs.
+    """
+    if n <= 0:
+        return 0
+    else:
+        return 1 + triple_step(n - 1) + triple_step(n - 2) + triple_step(n - 3)
