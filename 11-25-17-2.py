333451657f1a2ba13f7587664b538e3e7babe9a9
AnvilDev <andres.ernesto.valdes@gmail.com>
2017-11-25 23:44:19 -0500

    Another one.

---
+++ 11-25-17-2.py
@@ -0,0 +1,16 @@
+def twoSum(nums, target):
+    """
+    O(n) solution to find the indeces of two numbers in a list
+    that add up to a certain target
+
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    differences = {}
+
+    for i in range(len(nums)):
+        if nums[i] in differences:
+            return [differences[nums[i]], i]
+        else:
+            differences[target - nums[i]] = i
