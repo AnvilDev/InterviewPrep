8ace9c441fb38b9b3746e9967421e7e1db707be9
AnvilDev <andres.ernesto.valdes@gmail.com>
2017-12-17 19:13:45 -0500

    Commit.

--- Problems/11-23-17.py
+++ Problems/11-23-17.py
@@ -3,5 +3,7 @@ def is_subtree(root1, root2):
     Given two binary trees, write a method to compare if one is
     a subset of the other.
 
+    :root1 type: Node
+    :root2 type: Node
     """
     
