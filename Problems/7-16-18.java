28073819f11a0c3c9c57e357882a3d87a4efc87e
Andres Valdes <andres.ernesto.valdes@gmail.com>
2018-07-16 22:32:30 -0400

    Update 7-16-18.java

--- Problems/7-16-18.java
+++ Problems/7-16-18.java
@@ -25,3 +25,39 @@ class Solution {
         return mergedIntervals;
     }
 }
+// Note: This is significantly slower than a solution using a proper comparator:
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals == null) return null;
+        if (intervals.size() < 1) return intervals;
+        Collections.sort(intervals, new Comparator<Interval>(){
+            @Override
+            public int compare(Interval i1, Interval i2) {
+                if (i1.start > i2.start) {
+                    return 1;
+                } else if (i1.start < i2.start) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        List<Interval> mergedIntervals = new ArrayList<Interval>();
+        Interval prevInterval = new Interval(intervals.get(0).start, intervals.get(0).end);
+        for (Interval interval : intervals) {
+            if (prevInterval.end < interval.start || prevInterval.start > interval.end) {
+                mergedIntervals.add(prevInterval);
+                prevInterval = interval;
+            } else {
+                if (prevInterval.start <= interval.end) {
+                    prevInterval.start = Math.min(prevInterval.start, interval.start);
+                }
+                if (prevInterval.end >= interval.start) {
+                    prevInterval.end = Math.max(prevInterval.end, interval.end);
+                }
+            }
+        }
+        mergedIntervals.add(prevInterval);
+        return mergedIntervals;
+    }
+}
